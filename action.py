f0a3f99e4b7ef1df5e80cca0c083a41568e8c71b
Vibhuj01 <jainvibhu.2002@gmail.com>
2023-10-18 21:07:16 +0530

    Integrated GPT

---
+++ action.py
@@ -0,0 +1,95 @@
+import webbrowser
+import os
+import keyboard
+import platform
+import time
+import pyttsx3
+from chat_completetion import completion_api_call
+# init pyttsx
+engine = pyttsx3.init("sapi5") #The Speech Application Programming Interface or SAPI is an API developed by Microsoft to allow the use of speech recognition and speech synthesis within Windows applications
+voices = engine.getProperty("voices") # engine.getProperty gives us voice samples
+
+engine.setProperty('rate', 150) # Decrease the Speed Rate
+
+engine.setProperty('voice', voices[1].id)  # 1 for female and 0 for male voice
+
+def speak(audio):
+    engine.say(audio) # engine.say (listens to user input)
+    engine.runAndWait() # engine.runAndWait
+
+
+
+def take_action(keyword,query):
+
+    if keyword == 'youtube':
+    
+        speak("opening youtube")
+
+        if platform.system() == "Darwin":
+            webbrowser.get('macosx').open("https://youtube.com")
+
+        elif platform.system() == "Windows":
+            webbrowser.get('windows-default').open("https://youtube.com")
+
+    elif keyword == 'google':
+        speak("opening Google")
+
+        if platform.system() == "Darwin":
+            webbrowser.get('macosx').open("https://google.com")
+
+        elif platform.system() == "Windows":
+            webbrowser.get('windows-default').open("https://google.com")
+
+    elif  keyword == 'github':
+        speak("opening github")
+
+        if platform.system() == "Darwin":
+            webbrowser.get('macosx').open("https://github.com")
+
+        elif platform.system() == "Windows":
+            webbrowser.get('windows-default').open("https://github.com")
+
+    elif  keyword == 'teams':
+        speak("opening teams")
+
+        if platform.system() == "Darwin":
+            os.system("open /Applications/Teams.app")
+
+        elif platform.system() == "Windows":
+            loc = os.path.join(os.environ['USERPROFILE'], "AppData\\Local\\Microsoft\\Teams\\current\\Teams.exe")
+            os.startfile(loc)
+
+    elif keyword == 'spotify':
+        speak("opening spotify")
+
+        if platform.system() == "Darwin":
+            os.system("open /Applications/Spotify.app")
+
+        elif platform.system() == "Windows":
+            loc = os.path.join(os.environ['APPDATA'], "Spotify\\Spotify.exe")
+            os.startfile(loc)
+            time.sleep(1)
+            keyboard.press_and_release('Space')
+
+    elif keyword == 'whatsapp':
+        speak("opening whatsapp")
+
+        if platform.system() == "Darwin":
+            os.system("open /Applications/WhatsApp.app")
+
+        elif platform.system() == "Windows":
+            loc = os.path.join(os.environ['USERPROFILE'], "AppData\\Local\\WhatsApp\\WhatsApp.exe")
+            os.startfile(loc)
+
+    elif keyword == 'small_talk':
+        
+        context = f"You are a small talk module, just have fun with user by replying with a humorous answers [Query]: {query}"
+        completion = completion_api_call(context)
+        print(completion)
+        speak(completion)
+
+    elif keyword == 'exit':
+
+            print("Bye!!! it was nice meeting you.")
+            speak("Bye!!! it was nice meeting you.")
+            exit(0)
---
+++ chat_completetion.py
@@ -0,0 +1,48 @@
+import requests
+
+def completion_api_call(context: str) -> str:
+    """It makes API call to OpenAI's ChatGPT
+
+    Args:
+        context (str): context
+
+    Returns:
+        str: output based on context
+    """
+
+    api_key = "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"
+    endpoint = "https://api.openai.com/v1/chat/completions"
+
+    headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
+
+    payload = {"messages": [{"role": "user", "content": context}], "model": "gpt-3.5-turbo-0613", "temperature": 0}
+
+    response = requests.post(endpoint, headers=headers, json=payload)
+    response_data = response.json()
+    ans = response_data["choices"][0]["message"]["content"]
+    return ans
+
+
+def context_query(query):
+
+    context = f"""
+    [Role]: You are a voice assistant that helps us understand user's intent.
+
+    [Description]:
+    You will identify user's intent and give me appropraite output based on the following options:
+    - youtube: user wants to open or watch videos on youtube
+    - google: user wants to search things online or open google
+    - github: user wants to open github
+    - teams: user wants to open microsoft teams
+    - spotify: user wants to play music or open spotify
+    - whatsapp: user wants to open and use whatsapp
+    - exit: user wants to close the application
+    - small_talk: Detect this if none of the above are detected
+
+    [Instructions]:
+    - Classify user's intent and generate only 1 intent
+
+    [Query]: {query}
+    [Answer]:
+"""
+    return completion_api_call(context=context)
